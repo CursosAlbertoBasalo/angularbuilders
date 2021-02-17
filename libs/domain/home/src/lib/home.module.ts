777e84bad7c719c283c414da2d421b93a8ddb94f
Alberto Basalo <albertobasalo@hotmail.com>
2021-02-17 17:01:00 +0100

    feat: home services #5

--- libs/domain/home/src/lib/home.module.ts
+++ libs/domain/home/src/lib/home.module.ts
@@ -1,3 +1,5 @@
+import { DataModule } from '@ab/data';
+import { UiModule } from '@ab/ui';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -7,6 +9,8 @@ import { HomeService } from './home.service';
 @NgModule({
   imports: [
     CommonModule,
+    DataModule,
+    UiModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: HomePage },
     ]),
--- libs/domain/home/src/lib/home.service.ts
+++ libs/domain/home/src/lib/home.service.ts
@@ -1,1 +1,26 @@
-export class HomeService {}
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+interface ApiArray {
+  data: unknown[];
+}
+
+@Injectable()
+export class HomeService {
+  constructor(private http: HttpClient) {}
+  getCategories$() {
+    const url = 'https://angularbuilders-pre.herokuapp.com/api/v1/categories';
+    return this.http
+      .get<ApiArray>(url)
+      .pipe(map((results: ApiArray) => results.data));
+  }
+  getFeatured$() {
+    const url = 'https://angularbuilders-pre.herokuapp.com/api/v1/items';
+
+    return this.http.get<ApiArray>(url).pipe(
+      map((results: ApiArray) => results.data),
+      map((data: unknown[]) => data.slice(0, 3))
+    );
+  }
+}
