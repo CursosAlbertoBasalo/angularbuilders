4efe194d311d741bbfa150a2221d4185479cd4b4
Alberto Basalo <albertobasalo@hotmail.com>
2021-02-18 10:11:39 +0100

    feat: title-component-(atom) - Closes #10

--- libs/shared/ui/src/lib/ui.module.ts
+++ libs/shared/ui/src/lib/ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from './components/title/title.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [TitleComponent],
+  exports: [TitleComponent],
 })
 export class UiModule {}
