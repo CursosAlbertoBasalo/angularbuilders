160bef14df0ec80a6b248d0a4cdb3bf0f4d36a78
Alberto Basalo <albertobasalo@hotmail.com>
2021-02-22 08:33:59 +0100

    refactor: export components code at index

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -1,3 +1,5 @@
+export * from './lib/components/card/card.component';
+export * from './lib/components/title/title.component';
 export * from './lib/models/card';
 export * from './lib/models/title';
 export * from './lib/ui.module';
