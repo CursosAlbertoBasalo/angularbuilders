235ad6e48c61439e3137c85d0a49291349685244
Alberto Basalo <albertobasalo@hotmail.com>
2021-03-01 09:15:21 +0100

    refactor: logic to store

--- libs/shared/data/src/lib/audit.interceptor.ts
+++ libs/shared/data/src/lib/audit.interceptor.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { catchError, filter, tap } from 'rxjs/operators';
+import { filter, tap } from 'rxjs/operators';
 import { AuditStore } from './audit.store';
 
 @Injectable()
@@ -21,23 +21,26 @@ export class AuditInterceptor implements HttpInterceptor {
     if (request.method !== 'GET') {
       return next.handle(request);
     }
-    this.startLoading();
+
+    this.store.startLoading();
+    // this.store.dispatch('start-loading');
+
     return next.handle(request).pipe(
-      filter(() => request.method === 'GET'),
       filter((event) => event instanceof HttpResponse),
       tap({
         next: () => this.completeLoad(),
-      }),
-      catchError((error) => {
-        this.failedLoad(error.message);
-        throw error;
+        error: (error) => this.failedLoad(error.message),
       })
+      // catchError((error) => {
+      //   this.failedLoad(error.message);
+      //   throw error;
+      // })
     );
   }
 
-  private startLoading() {
-    this.store.setState({ isLoading: true, errorMessage: '' });
-  }
+  // private startLoading() {
+  //   this.store.setState({ isLoading: true, errorMessage: '' });
+  // }
 
   private completeLoad() {
     this.store.setState({ isLoading: false, errorMessage: '' });
--- libs/shared/data/src/lib/audit.store.ts
+++ libs/shared/data/src/lib/audit.store.ts
@@ -9,4 +9,19 @@ export class AuditStore extends Store<Audit> {
   constructor() {
     super({ isLoading: false, errorMessage: '' });
   }
+
+  public startLoading() {
+    this.setState({ isLoading: true, errorMessage: '' });
+  }
+
+  public dispatch(type: string, payload?: any) {
+    switch (type) {
+      case 'start-loading':
+        this.setState({ isLoading: true, errorMessage: '' });
+        break;
+
+      default:
+        break;
+    }
+  }
 }
