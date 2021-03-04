e7d955dcd91ac73343edd80f9ac9a1984e7c900b
Alberto Basalo <albertobasalo@hotmail.com>
2021-03-04 10:17:23 +0100

    feat: pwa-add-to-home-screen - Closes #60

--- apps/catalog-web/src/app/app.component.ts
+++ apps/catalog-web/src/app/app.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'ab-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public promptEvent!: { preventDefault: any; prompt: () => void };
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(event: { preventDefault: any; prompt: () => void }) {
+    event.preventDefault();
+    this.promptEvent = event;
+  }
+
+  public installPWA() {
+    this.promptEvent.prompt();
+  }
+
+  public shouldInstall(): boolean {
+    return !this.isRunningStandalone() && this.promptEvent !== undefined;
+  }
+
+  public isRunningStandalone(): boolean {
+    return window.matchMedia('(display-mode: standalone)').matches;
+  }
+}
