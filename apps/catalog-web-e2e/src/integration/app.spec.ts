7df084b36ba85cdcb8e626e5cce71560b05b4ad5
Alberto Basalo <albertobasalo@hotmail.com>
2021-03-04 11:17:34 +0100

    feat: e2e-initial - Closes #64

--- apps/catalog-web-e2e/src/integration/app.spec.ts
+++ apps/catalog-web-e2e/src/integration/app.spec.ts
@@ -1,13 +1,12 @@
-import { getGreeting } from '../support/app.po';
+import { getTitle } from '../support/app.po';
 
 describe('catalog-web', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to catalog-web!');
+  it('should display the Title', () => {
+    getTitle().contains('Angular Builders');
+  });
+  it('should log in', () => {
+    cy.login('albertobasalo@hotmail.com');
   });
 });
--- apps/catalog-web-e2e/src/support/commands.ts
+++ apps/catalog-web-e2e/src/support/commands.ts
@@ -1,33 +1,14 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
-    login(email: string, password: string): void;
+    login(email: string): void;
   }
 }
-//
-// -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
+
+Cypress.Commands.add('login', (email) => {
+  cy.get('input[name="email"]').type(email);
+  cy.get('button').contains('Send me a token').click();
+  cy.url().should('contain', 'activate');
+  cy.get('button').contains('Activate me').click();
 });
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
